13943eb6f123783dce48a9dab2f0678b8361345a
saim_alam1 <allbehassansaim@gmail.com>
2025-04-06 15:18:46 +0600

    Setuped Environment and Created NavBar

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+
+      <Navbar></Navbar>
+
+      <Blogs></Blogs>
+
+    </>
+  )
+}
+
+export default App
